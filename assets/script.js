6b070c2682db72aae2bdbeecc6b23aff2569bbba
Matty Party <matthewpewewardy@gmail.com>
2020-10-01 22:05:30 -0500

    basic html and java page

---
+++ assets/script.js
@@ -0,0 +1,94 @@
+
+//-------------------------------
+// var tElm = document.getElementById("title");
+// var qElm = document.getElementById("questions");
+
+// var prev = document.getElementById('prev');
+// var next = document.getElementById('next');
+
+// var pos = 0;
+// var grade = [];
+
+var questions = [
+  {
+    title: "Question 1",
+    options: ["Answer 1", "Answer 2", "Answer 3"],
+    answer: "Answer 1",
+  },
+  {
+    title: "Question 2",
+    options: ["Answer a", "Answer b", "Answer c"],
+    answer: "Answer a",
+  },
+  {
+    title: "Question 3",
+    options: ["Answer 1a", "Answer 2b", "Answer 3c"],
+    answer: "Answer 1a",
+  },
+];
+
+
+//-------------------------------
+var body = document.body;
+var h1El = document.createElement("h1");
+var h2El = document.createElement("h2");
+var btn = document.createElement("button");
+h1El.textContent = questions[0].title;
+h2El.textContent = "h2 tag"
+btn.textContent="button text"
+body.appendChild(h1El);
+body.appendChild(h2El)
+body.appendChild(btn);
+
+
+// //-------------------------------
+// prev.addEventListener('click', moveBackwards, false);
+// next.addEventListener('click', moveFoward, false);
+
+// function moveFoward (evt) {
+//   pos = (pos === (questions.length - 1)) ? 0 : pos + 1;
+//   draw();
+// }
+
+// function moveBackwards(evt) {
+//   pos = (pos === 0) ? questions.length - 1 : pos - 1;
+//   draw();
+// }
+
+// function draw () {
+//   var deck = questions[pos];
+
+//   updateTitle(deck.title);
+//   updateOptions(deck.options);
+// }
+
+// //-------------------------------
+// qElm.addEventListener('click', optionPicked, false);
+
+// function optionPicked(evt) {
+//   var val = evt.target.innerText;
+//   var deck = questions[pos];
+
+//   grade[pos] = (val === deck.answer) ? 1 : 0;
+
+//   moveFoward();
+// }
+
+// //-------------------------------
+// function updateTitle (title) {
+//   tElm.innerText = title;
+// }
+
+// function updateOptions(options) {
+//   qElm.innerText = '';
+
+//   for (var i = 0; i < options.length; i++) {
+//     var txt = document.createTextNode(options[i]);
+//     var btn = document.createElement('button');
+
+//     btn.appendChild(txt);
+//     qElm.appendChild(btn);
+//   }
+// }
+
+// updateQuestions(["Answer 1", "Answer 2", "Answer 3"]);
---
+++ psuedocode.md
@@ -0,0 +1,32 @@
+# 04_web_api_quiz
+
+Start button to start the quick
+    on click of start button timer starts
+    on click of start first question appears
+
+List High Scores on start page
+    button to clear high scores
+
+
+# Loop Through all of these till the last question
+
+first question
+    first question (question stored in array)
+        four multiple choice answers
+            One of 4 is correct answer
+* On wrong choice time is subtracted and note try again
+* On correct choise "Correct" is prompted and move on to next question 
+
+
+# When Timer runs out game ends
+
+# When Game Ends prompt user for initals and store in local storage
+
+
+
+
+
+# Mouse over Events
+    change color of buttom when mouse 
+
+# Store high score in Local Storage  
